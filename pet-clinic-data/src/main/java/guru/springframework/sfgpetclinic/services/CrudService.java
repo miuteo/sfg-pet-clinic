9b941d7731ba14b614c1eff55d60aaccfa20961e
Teo Miu - Asus x556 <miu.teo@gmail.com>
2020-04-17 19:10:13 +0300

    refactor services to common interface

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/CrudService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/CrudService.java
@@ -1,4 +1,13 @@
 package guru.springframework.sfgpetclinic.services;
 
-public interface CrudService {
+import guru.springframework.sfgpetclinic.model.Owner;
+
+import java.util.Set;
+
+public interface CrudService<T,ID> {
+    Set<T> findAll();
+    T findById(ID id);
+    T save(T t);
+    void delete(T t);
+    void deleteById(ID id);
 }
