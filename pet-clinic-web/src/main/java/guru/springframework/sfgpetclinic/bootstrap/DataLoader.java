c07ab21cc046c382b6e339950803c47ca97c04b4
Teo Miu - Asus x556 <miu.teo@gmail.com>
2020-04-19 20:44:13 +0300

    list vets

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -47,7 +47,7 @@ public class DataLoader implements CommandLineRunner {
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(1L);
+        vet2.setId(2L);
         vet2.setFirstName("vet2FirstName");
         vet2.setLastName("vet2LastName");
         vetService.save(vet2);
