ba9cad02940fc3e79a55049a3c0b8f26c965599f
Teo Miu - Asus x556 <miu.teo@gmail.com>
2020-04-27 09:49:27 +0300

    create speciality map service

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/SpecialityServiceMap.java
@@ -0,0 +1,7 @@
+package guru.springframework.sfgpetclinic.services.map;
+
+import guru.springframework.sfgpetclinic.model.Speciality;
+import guru.springframework.sfgpetclinic.services.SpecialityService;
+
+public class SpecialityServiceMap extends AbstractMapService<Speciality,Long> implements SpecialityService {
+}
