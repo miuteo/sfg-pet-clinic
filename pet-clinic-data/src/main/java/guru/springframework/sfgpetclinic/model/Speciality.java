0c1f877fb6de7ad5e29043e1ee71bde962e20830
Teo Miu - Asus x556 <miu.teo@gmail.com>
2020-04-30 00:06:49 +0300

    vets to jpa

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Speciality.java
@@ -1,6 +1,15 @@
 package guru.springframework.sfgpetclinic.model;
 
+import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
 public class Speciality extends BaseEntity{
+
+    @ManyToMany(mappedBy = "specialities")
+    private Set<Vet> vets = new HashSet<>();
     private String description;
 
     public String getDescription() {
--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
@@ -1,9 +1,15 @@
 package guru.springframework.sfgpetclinic.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
 public class Vet extends Person{
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "vets_specialiesX",
+            joinColumns = @JoinColumn(name="vet_idX"),
+            inverseJoinColumns = @JoinColumn(name="spec_id_X"))
     private Set<Speciality> specialities = new HashSet<>();
 
     public Set<Speciality> getSpecialities() {
